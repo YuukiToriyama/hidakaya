09a9a2916fd2472c1eea0a6c5d134bcc2d3314d1
YUUKIToriyama <github@toriyama.dev>
2022-06-25 22:26:16 +0900

    Promise.allで並列化し処理を高速化

--- src/main.ts
+++ src/main.ts
@@ -8,32 +8,35 @@ import { Menu } from "./Model/Menu";
 	await fs.mkdir("./output/shop", { recursive: true });
 
 	// メニュー一覧を取得
+	// 1. カテゴリごとにJSONを作成
+	// 2. 全メニューを収録したJSONを作成
 	let menuList: Menu[] = [];
 	for (let i = 1; i <= 8; i++) {
 		const url = `https://hidakaya.hiday.co.jp/hits/ja/menu/2/list/${i}.html`;
 		const response = await fetchMenu(url);
 		const fileName = `./output/menu/${i}.json`;
-		await fs.writeFile(fileName, JSON.stringify(response, null, "\t"));
-		console.log(`${fileName} was created.`);
+		await writeJSONFile(fileName, response);
+
 		menuList = menuList.concat(response.menu);
 	}
-	await fs.writeFile(
-		"./output/menu/all.json",
-		JSON.stringify(menuList, null, "\t")
-	);
-	console.log(`./output/menu/all.json was created.`);
+	await writeJSONFile("./output/menu/all.json", menuList);
 
 	// 店舗一覧を取得
+	// 1. 店舗一覧JSONを作成
+	// 2. 各店舗の情報を持ったJSONを作成
 	let shopList = await fetchShopList();
-	await fs.writeFile(
-		"./output/shop/all.json",
-		JSON.stringify(shopList, null, "\t")
-	);
-	console.log("./output/shop/all.json was created.");
-	for (let shop of shopList) {
-		const shopInfo = await fetchShopInfo(shop.id);
+	await writeJSONFile("./output/shop/all.json", shopList);
+	const taskList = shopList.map(shop => (async () => {
 		const fileName = `./output/shop/${shop.id}.json`;
-		await fs.writeFile(fileName, JSON.stringify(shopInfo, null, "\t"));
-		console.log(`${fileName} was created`);
-	}
+		const shopInfo = await fetchShopInfo(shop.id);
+		await writeJSONFile(fileName, shopInfo);
+	})());
+	await Promise.all(taskList);
 })();
+
+const writeJSONFile = async (fileName: string, object: Object) => {
+	const json = JSON.stringify(object, null, "\t");
+	const byteLength = Buffer.byteLength(json);
+	await fs.writeFile(fileName, json)
+	console.log(`${fileName} was created (${byteLength} byte).`)
+}

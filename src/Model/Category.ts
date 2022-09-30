882cfe267545f48584fbdee62ae354ff0b72f577
YUUKIToriyama <github@toriyama.dev>
2022-09-30 16:29:07 +0900

    add Model/Category.ts

---
+++ src/Model/Category.ts
@@ -0,0 +1,17 @@
+export interface Category {
+	/** ID */
+	id: number
+	/** カテゴリ名 */
+	name: string
+}
+
+export const categories: Category[] = [
+	{ id: 1, name: "ラーメン" },
+	{ id: 2, name: "セット" },
+	{ id: 3, name: "定食" },
+	{ id: 4, name: "単品" },
+	{ id: 5, name: "おつまみ" },
+	{ id: 6, name: "ドリンク" },
+	{ id: 7, name: "テイクアウト" },
+	{ id: 8, name: "トッピング" }
+];
